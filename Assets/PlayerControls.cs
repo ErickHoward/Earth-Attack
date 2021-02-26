dfd891906f1cc800934c8177a992581c5ced61ed
Erick Howard <goldkillerracer2@gmail.com>
2021-02-26 17:54:46 +0800

    Clamped player movement

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -7,6 +7,8 @@ public class PlayerControls : MonoBehaviour
 {
     [SerializeField] InputAction movement;
     [SerializeField] Vector2 controlSpeed = new Vector2(30f,30f);
+    [SerializeField] Vector2 playerShipXRange; // x value is how far left the ship can go, should be a minus number, y is how far right the ship can go, should be a positive number
+    [SerializeField] Vector2 playerShipYRange; // x value is how far down the ship can go, should be a minus number, y is how far up the ship can go, should be a positive number
     // Start is called before the first frame update
     void Start()
     {
@@ -28,14 +30,17 @@ public class PlayerControls : MonoBehaviour
         float yThrow = movement.ReadValue<Vector2>().y;
 
         float xOffset = xThrow * Time.deltaTime * controlSpeed.x;
-        float newXPos = transform.localPosition.x + xOffset;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, playerShipXRange.x, playerShipXRange.y);
 
         float yOffset = yThrow * Time.deltaTime * controlSpeed.y;
-        float newYPos = transform.localPosition.y + yOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, playerShipYRange.x, playerShipYRange.y);
+
         transform.localPosition = new Vector3 
         (
-            newXPos,
-            newYPos,
+            clampedXPos,
+            clampedYPos,
             transform.localPosition.z
         );
         
