69b44594d94f3cca3d0dd4c403ab1db41a7f3b2b
Erick Howard <goldkillerracer2@gmail.com>
2021-03-16 20:49:41 +0800

    Scoreboard UI updates based on score

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Scoreboard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+    private void Start() 
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "No Points Yet";
+        
+    }
 
     public void UpdateScore(int amountToIncrease)   
     {
         score += amountToIncrease;
-        Debug.Log($"Score is now: {score}");
-
+        scoreText.text = score.ToString();
     }
 }
